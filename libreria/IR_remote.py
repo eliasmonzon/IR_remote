714dfb0b961b681d62736442e69133731ab2fc1c
electronica y diagramas <151683295+eliasmonzon@users.noreply.github.com>
2024-03-15 21:56:09 -0300

    Update IR_remote.py

--- libreria/IR_remote.py
+++ libreria/IR_remote.py
@@ -6,8 +6,8 @@ class ir_receptor(object):
     def __init__(self, gpioNum):
         self.irRecv = machine.Pin(gpioNum, machine.Pin.IN, machine.Pin.PULL_UP)
         self.irRecv.irq(
-            trigger=machine.Pin.IRQ_RISING | machine.Pin.IRQ_FALLING,
-            handler=self.__logHandler)
+            trigger = machine.Pin.IRQ_RISING | machine.Pin.IRQ_FALLING,
+            handler = self.__logHandler)
         self.logList = []
         self.index = 0
         self.start = 0
@@ -26,28 +26,13 @@ class ir_receptor(object):
 
     def ir_read(self):
         utime.sleep_ms(200) 
-        if utime.ticks_diff(
-                utime.ticks_us(),
-                self.start) > 800000 and self.index > 0:  #10ms执行一次
-            ir_buffer=[]
-       
-            for i in range(3,66,2):
-                if self.logList[i]>800:
-                    ir_buffer.append(1)
-                else:
-                    ir_buffer.append(0)
-            irValue=0x00000000
-            for i in range(0,4):
-                for j in range(0,8):
-                    if ir_buffer[i*8+j]==1:
-                        irValue=irValue<<1
-                        irValue |= 0x01
-                    else:
-                        irValue=irValue<<1
-                        irValue &= 0xfffffffe                    
-            # reset 
-            self.logList = []
-            self.index = 0
-            self.start = 0
-            return hex(irValue)
-       
+        if utime.ticks_diff(utime.ticks_us(), self.start) > 800000 and self.index > 0:
+           ir_buffer = [1 if duration > 800 else 0 for duration in self.logList[3:66:2]]
+           irValue = 0
+           for bit in ir_buffer:
+               irValue = (irValue << 1) | bit
+           # Reiniciar los valores
+           self.logList = []
+           self.index = 0
+           self.start = 0
+           return hex(irValue)
